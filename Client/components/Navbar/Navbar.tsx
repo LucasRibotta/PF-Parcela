bab5edd9f187bd6a34dade8ecc0f1fd43a3f7f73
Luciano <lucianoalejandromas@gmail.com>
2023-06-15 13:08:49 -0300

    "adding links"

--- Client/components/Navbar/Navbar.tsx
+++ Client/components/Navbar/Navbar.tsx
@@ -7,6 +7,15 @@ export default function Navbar() {
         <li>
           <Link href="/home">Home</Link>
         </li>
+        <li>
+          <Link href="/gallery">Gallery</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
       </ul>
     </div>
   )
