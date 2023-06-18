1eab4af989438a32c13ad50bfd525fbc54eb1f0e
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-06-18 03:14:58 -0500

    feat(HomeSection): component rendering ServiceSection and Footer

--- Client/components/HomeSection/HomeSection.tsx
+++ Client/components/HomeSection/HomeSection.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image"
 import forestImage from "../../img/forestImage.jpg"
+import ServiceSection from "../services-section/ServiceSection"
+import Footer from "../footer/Footer"
 export default function HomeSection() {
   return (
-    <div className="absolute inset-0">
-      <Image src={forestImage} alt="#" className="w-full h-full object-cover" />
-    </div>
+    <>
+      <div className="h-[100%]">
+        <Image src={forestImage} alt="#" className="w-full h-full object-cover fixed z-[-1]" />
+        <ServiceSection />
+        <Footer />
+      </div>
+    </>
   )
 }
