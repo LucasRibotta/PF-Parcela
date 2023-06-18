84850802ee626e16fbcf33b4077ae2d5ec8265d9
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-06-18 03:10:14 -0500

    feat(app/page): render homeSection

--- Client/app/page.tsx
+++ Client/app/page.tsx
@@ -1,7 +1,7 @@
 import HomeSection from "@/components/HomeSection/HomeSection"
 export default function Home() {
   return (
-    <div className="h-screen bg-black opacity-90 brightness-50 contrast-90">
+    <div className="h-[100%] text-white">
       <HomeSection />
     </div>
   )
