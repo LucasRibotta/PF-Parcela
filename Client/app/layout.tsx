6885ab4847d4660348f5e302b6f18704bebedbd8
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-06-17 21:03:16 -0500

    remove(globals/layout/page): global stiles and html basic of next

--- Client/app/layout.tsx
+++ Client/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link href="/dist/output.css" rel="stylesheet"></link>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
