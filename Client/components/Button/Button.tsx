eee63d96a598f78472029b741e7ee58b5ad6b871
Luciano <lucianoalejandromas@gmail.com>
2023-06-18 03:04:00 -0300

    "refactor: <Button> = "typescript sintax changed"

--- Client/components/Button/Button.tsx
+++ Client/components/Button/Button.tsx
@@ -1,13 +1,11 @@
-interface ButtonProps {
+type ButtonProps = {
   text: string
 }
 
-const Button: React.FC<ButtonProps> = ({ text }) => {
+export default function Button({ text }: ButtonProps) {
   return (
     <button className="bg-transparent border-white border-[1px] text-white hover:bg-[#51a8a1]  ease-in-out duration-300 px-4 py-[6px] rounded">
       {text}
     </button>
   )
 }
-
-export default Button
