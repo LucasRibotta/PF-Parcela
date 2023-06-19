7de7321b74577e22ff6ae4bd74990c113c852b57
Luciano <lucianoalejandromas@gmail.com>
2023-06-18 21:47:50 -0300

    "refactor: <Nav> = image changes and video bg"

--- Client/components/HomeSection/HomeSection.tsx
+++ Client/components/HomeSection/HomeSection.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image"
-import forestImage from "../../img/forestImage.jpg"
 export default function HomeSection() {
   return (
     <div className="absolute inset-0">
-      <Image src={forestImage} alt="#" className="w-full h-full object-cover" />
+      <video autoPlay loop muted className="w-full h-full object-cover">
+        <source src="/homeVideo.mp4" type="video/mp4" />
+      </video>
     </div>
   )
 }
--- Client/components/Navbar/Navbar.tsx
+++ Client/components/Navbar/Navbar.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link"
 import Image from "next/image"
-import logo from "../../img/logo.png"
+import logo from "../../img/logoIcon.png"
 import Button from "../Button/Button"
 
 export default function Navbar() {
   return (
     <nav className="flex fixed items-center justify-between p-[0.50rem] px-[3rem] z-[1] w-full">
-      <Image src={logo} alt="#" className="h-8 w-8" />
+      <div>
+        <Image src={logo} alt="#" className="w-[6rem] " />
+      </div>
       <ul className="flex justify-end gap-[2rem] items-center text-white">
-        <li className="px-[22px] py-[20px]">
+        <li className=" px-[22px] py-[20px]">
           <Link
             href="/"
-            className="hover:text-[#51a8a1]  ease-in-out duration-300"
+            className=" hover:text-[#51a8a1]  ease-in-out duration-300 "
           >
             Home
           </Link>
@@ -41,7 +43,7 @@ export default function Navbar() {
           </Link>
         </li>
       </ul>
-      <div className="flex gap-2">
+      <div className="flex gap-2 ">
         <Button text="Sing in" />
       </div>
     </nav>
