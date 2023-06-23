560f0ff5514bdcf8d079951c09f4df139a155471
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-06-22 22:55:16 -0500

    fix(UploadImage): changes button and add images component

--- Client/components/UploadImage/UploadImage.tsx
+++ Client/components/UploadImage/UploadImage.tsx
@@ -1,5 +1,6 @@
 import React, { useState, ChangeEvent } from 'react';
 import Axios from 'axios';
+import { Image, Video, Transformation } from 'cloudinary-react';
 
 export default function UploadImage() {
   const [imagesSelected, setImagesSelected] = useState<File[]>([]);
@@ -10,16 +11,16 @@ export default function UploadImage() {
       console.log('No image has been selected');
       return;
     }
-  
+
     const uploadPromises = imagesSelected.map((image) => {
       const formData = new FormData();
       formData.append('file', image);
       formData.append('upload_preset', 'parcelasImg');
       formData.append('folder', 'Parcelas');
-  
+
       return Axios.post('https://api.cloudinary.com/v1_1/parcelas/image/upload', formData);
     });
-  
+
     Promise.all(uploadPromises)
       .then((responses) => {
         console.log(responses);
@@ -28,23 +29,36 @@ export default function UploadImage() {
         console.log('Error uploading images to Cloudinary', error);
       });
   };
-  
 
-  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (files) {
-      const selectedImages = Array.from(files);
-      setImagesSelected(selectedImages);
-    }
+
+  const handleButtonClick = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.multiple = true;
+    input.accept = 'image/*';
+    input.onchange = (event) => {
+      const files = (event.target as HTMLInputElement)?.files;
+      if (files) {
+        const selectedImages = Array.from(files);
+        setImagesSelected(selectedImages);
+      }
+    };
+    input.click();
   };
-  
+
 
   return (
     <div className='flex flex-col text-current text-white'>
-      <input type="file" onChange={handleImageChange} multiple />
+      <button className='border-black rounded-lg cursor-pointer bg-white w-[30%] h-[50%] m-auto' onClick={handleButtonClick}>oprime esto y te mueres</button>
+      {imagesSelected.length > 0 && <p>{imagesSelected.length} imágenes seleccionadas</p>}
 
       <button className='border-black rounded-lg cursor-pointer bg-white w-[30%] h-[50%] m-auto ' onClick={uploadImg}>Upload Images</button>
 
+      <Image cloudName="demo" publicId="sample">
+        <Transformation angle="-45" />
+        <Transformation effect="trim" angle="45" crop="scale" width="600" />
+        <Transformation overlay="text:Arial_100:Hello" />
+      </Image>
 
     </div>
   );
