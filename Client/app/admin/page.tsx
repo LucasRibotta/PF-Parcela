aa85cd8fbd297c1792e2192d29ecb94cc4393d19
gonzaliaga <gonzaliaga@gmail.com>
2023-06-25 20:31:08 -0400

    Gonza

--- Client/app/admin/page.tsx
+++ Client/app/admin/page.tsx
@@ -1,6 +1,6 @@
 export default function Admin() {
   return (
-<div className="pt-[6.8rem] h-screen flex bg-gray-800">
+<div className="pt-[5.5rem] h-screen flex bg-gray-800">
       <div
         className="flex flex-col fixed items-center w-40 h-screen overflow-hidden text-gray-400 bg-gray-900 rounded"
         style={{ height: "calc(100vh - 7rem - 1.9rem)" }}
@@ -14,7 +14,7 @@ export default function Admin() {
           >
             <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
           </svg>
-          <span className="ml-2 text-sm font-bold">The App</span>
+          <span className="ml-2 text-sm font-bold">Admin</span>
         </a>
         <div className="w-full  px-2">
           <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
@@ -36,7 +36,7 @@ export default function Admin() {
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-              <span className="ml-2 text-sm font-medium">Dasboard</span>
+              <span className="ml-2 text-sm font-medium">Dashboard</span>
             </a>
             <a
               className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
