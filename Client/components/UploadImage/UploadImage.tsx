e560a7b9a3c1a5b5b82c242418f70433b891e1e4
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-06-26 00:44:22 -0500

    feat(FormSection/Maps/UploadImage): CSS FormSection and Maps

--- Client/components/UploadImage/UploadImage.tsx
+++ Client/components/UploadImage/UploadImage.tsx
@@ -29,7 +29,7 @@ export default function UploadImage() {
       .catch((error) => {
         console.log('Error uploading images to Cloudinary', error);
       });
-    };
+  };
 
 
   const handleButtonClick = () => {
@@ -49,24 +49,21 @@ export default function UploadImage() {
 
 
   return (
-    <div className='flex flex-col text-current text-black m-auto' >
+    <div className='flex flex-col justify-center items-start text-black' >
 
-      <div className='border-black rounded-lg cursor-pointer bg-white w-[25%] h-[50%] m-auto mb-[1rem] ' >
-      <button type='button' onClick={handleButtonClick}>Añadir Archivo</button>
-      {imagesSelected.length > 0 && <p>{imagesSelected.length} Image</p>}
+      <div className=' flex justify-center rounded-lg cursor-pointer w-[100%] mb-[1rem] ' >
+        <button className='py-2 px-4 bg-[#51a8a1] m-2 rounded-md text-white transition-all duration-300 hover:bg-[#126e67] hover:font-semibold' type='button' onClick={handleButtonClick}>Añadir Imagen</button>
+        <button className='py-2 px-4 bg-[#51a8a1] m-2 rounded-md text-white transition-all duration-300 hover:bg-[#126e67] hover:font-semibold' type='button' onClick={uploadImg}>Subir Imagen</button>
+        {imagesSelected.length > 0 && <p>{imagesSelected.length} Image</p>}
       </div>
 
-      <div className='border-black rounded-lg cursor-pointer bg-white w-[25%] h-[50%] m-auto '>
-      <button type='button' onClick={uploadImg}>Cargar Imagen</button>
-      </div>
-
-      <div className='flex flex-row w-[15%] h-[15%] ml-[3rem] mt-[2rem] '>
-      <Image cloudName="parcelas" publicId="https://res.cloudinary.com/parcelas/image/upload/v1687573942/0d66c79d-ad42-4bad-b8e1-43b1228b2e37_d3uzal.jpg" alt='Carga Imagen'>
-        <Transformation angle="-45" />
-        <Transformation effect="trim" angle="45" crop="scale" width="600" />
-        <Transformation overlay="text:Arial_100:Hello" />
-      </Image>
-      </div>
+      {/* <div className='flex flex-row w-[15%] h-[15%] ml-[3rem] mt-[2rem] '>
+        <Image cloudName="parcelas" publicId="https://res.cloudinary.com/parcelas/image/upload/v1687573942/0d66c79d-ad42-4bad-b8e1-43b1228b2e37_d3uzal.jpg" alt='Carga Imagen'>
+          <Transformation angle="-45" />
+          <Transformation effect="trim" angle="45" crop="scale" width="600" />
+          <Transformation overlay="text:Arial_100:Hello" />
+        </Image>
+      </div> */}
 
     </div>
   );
