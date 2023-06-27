178e9a2b7694fc9e544ba98b41977429562f0384
gonzaliaga <gonzaliaga@gmail.com>
2023-06-27 16:18:46 -0400

    Gonzalo Cambios Json

--- Client/components/footer/Footer.tsx
+++ Client/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="flex items-center w-[300px] sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] 2xl:w-[1536px] mx-auto justify-between min-h-[100px] bg-gray-100 rounded-t-2xl opacity-50 text-black font-bold mt-4 ">
+        <footer className="flex items-center w-[300px] sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] 2xl:w-[1536px] mx-auto justify-between min-h-[100px] bg-gray-400 rounded-t-2xl opacity-50 text-black font-bold mt-4 ">
             <div className="p-4 w-[33%]  ">
                 <h3>Preguntas</h3>
 
@@ -15,7 +15,7 @@ const Footer = () => {
                 </ul>
             </div>
             <div className="p-4 w-[33%] ">
-                <h3>About</h3>
+                <h3>Acerca</h3>
             </div>
         </footer>
     )
