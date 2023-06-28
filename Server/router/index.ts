e1247fe9512038b452a7944c51ba8b7468ab74ab
Ismael <ismael2095951@gmail.com>
2023-06-28 19:26:14 -0300

    fix(router/index) updated authentication routes

--- Server/router/index.ts
+++ Server/router/index.ts
@@ -1,5 +1,5 @@
 
-import {Router, Response, Request} from 'express'
+import {Router} from 'express'
 
 import authRoutes from "./auth";
 import authentication from './authentication'
@@ -9,34 +9,38 @@ const router = Router();
 router.use('/', authentication)
 
 router.get('/profile', (req,res) => {
+    
     const userAuth = {    
-        user: req.app.locals.user.email,
-        error: req.app.locals.signinMessage
-    }
+        user: req.app.locals.user.name,
+        accessLevel: req.app.locals.user.accessLevel,
+        loggedIn: true,
+        error: []
+    }  
     res.status(200).json(userAuth)
 })
 
-router.get('/profileUp', (req,res) => {
+router.get('/profileFail', (req,res) => {
+    const usuario = req.flash('error')[0]
+    let usuarioFail = ''  
+    if(res.locals.signinMessage.length !== 0) usuarioFail = res.locals.signinMessage[0]
+    else usuarioFail = res.locals.signupMessage[0]
+
     const userAuthU = {     
-        user: req.app.locals.user.email,
-        error: req.app.locals.signupMessage
-    }
+        user: usuario,
+        loggedIn: true,
+        error: usuarioFail
+    }   
     res.status(200).json(userAuthU)  
 })
 
-router.use((req,res,next) => {
-    isAuthenticated(req,res,next)
-    next()
+router.get('/logout', (req,res,next)=>{ 
+       
+    req.logOut(function(err) {
+        if(err) next(err)
+        res.status(200).json({user:'vacio', error: []})
+    })
 })
 
 router.use("/auth", authRoutes)
 
-function isAuthenticated(req: Request, res: Response, next:any) {
-    if(req.isAuthenticated()) {
-        return next()
-    }
-    res.status(404).json({message: 'requiere loguearse'})
-}
-
 export default router;
-
