7dba7f1b6b5d946bed83363bdd53a7463c7e8fa1
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-06-28 15:54:07 -0500

    feat(hero): add hero and CSS basic

--- Client/app/detail/page.tsx
+++ Client/app/detail/page.tsx
@@ -1,9 +1,13 @@
 import Card from "@/components/Card/Card"
 import DetailSection from "@/components/detail/Detail";
+import Hero from "@/components/hero/Hero";
 import 'tailwindcss/tailwind.css';
 
 export default function Detail() {
   return (
-    <DetailSection/>
+    <>
+      <DetailSection />
+      <Hero />
+    </>
   )
 }
---
+++ Client/components/hero/Hero.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+function Hero() {
+  return (
+          <div className="hero fixed top-0 left-0 bg-white bg-opacity-50 inset-0 overflow-hidden select-none pointer-events-none blur-lg -z-10 ">
+              <div className="ell one fixed rounded-full mix-blend-screen animation"></div>
+              <div className="ell two fixed rounded-full mix-blend-screen animation"></div>
+              <div className="ell three fixed rounded-full mix-blend-screen animation"></div>
+          </div>
+  )
+}
+
+export default Hero
