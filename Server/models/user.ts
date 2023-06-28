71610bde898f6264a5753ad6cf52cab7ad515857
Ismael <ismael2095951@gmail.com>
2023-06-28 19:32:58 -0300

    fix(models/user) user model and encryption

--- Server/models/user.ts
+++ Server/models/user.ts
@@ -3,6 +3,10 @@ import bcrypt from 'bcrypt'
 
 interface User  {
     id: number;
+    name: string;
+    lastname: string;
+    phone: number;
+    date: string;
     email: string;
     password: string;
     accessLevel: number;
@@ -12,6 +16,10 @@ interface User  {
 
 const userSchema = new Schema<User> ({
     id: {type: Number},
+    name: {type: String, required: true},
+    lastname: {type: String, required: true},
+    phone: {type: Number, required: false},
+    date: {type: String, required: true},
     email:{type: String, required: true, unique: true},
     password: {type: String, required: true },
     accessLevel: {type: Number, required: false, default: 1},
