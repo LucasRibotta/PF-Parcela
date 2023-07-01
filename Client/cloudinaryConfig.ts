2602aa84d86d028e8f01a615b0eadce1d324da62
LucasRibotta <lucasribotta15@gmail.com>
2023-07-01 15:36:59 -0300

    fix client config ok

--- Client/cloudinaryConfig.ts
+++ Client/cloudinaryConfig.ts
@@ -1,14 +1,7 @@
 import { config } from "cloudinary-react";
 
 config({
-<<<<<<< HEAD
     cloud_name: "parcelas",
     api_key: "972663226865153",
     api_secret: "3F6KfZwmamiq5ImEB3BYN5XdBVg",
   });
-=======
-  cloud_name: "parcelas",
-  api_key: "972663226865153",
-  api_secret: "3F6KfZwmamiq5ImEB3BYN5XdBVg",
-});
->>>>>>> 82c050d0977362d54f7121a0873b414a241d7487
