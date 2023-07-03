1abb9549253a4d76ee037eb2417a978c871fe9f5
AgustinaKravsiuk <agustinakravsiuk@gmail.com>
2023-07-03 18:59:20 -0300

    refactor(redux/features/parcelSlice): adding logic function

--- Client/redux/features/parcelSlice.ts
+++ Client/redux/features/parcelSlice.ts
@@ -15,12 +15,20 @@ interface Parcela {
 
 interface ParcelasState {
   allParcelas: Parcela[]
-  parcelas: Parcela[]
+  parcelas: Parcela[] 
+  priceRange: {
+    minPrice: number
+    maxPrice: number
+  } 
 }
 
 const initialState: ParcelasState = {
   allParcelas: [],
   parcelas: [],
+  priceRange: {
+    minPrice: 0,
+    maxPrice: 60000000
+  }
 }
 
 const parcelasSlice = createSlice({
@@ -34,14 +42,31 @@ const parcelasSlice = createSlice({
     sortParcelas: (state, action: PayloadAction<string>) => {
       const orderBy = action.payload
       if (orderBy === "asc") {
-        state.parcelas.sort((a, b) => a.price - b.price)
+        state.parcelas.sort((a, b) => Number(a.price) - Number(b.price))
       } else if (orderBy === "desc") {
-        state.parcelas.sort((a, b) => b.price - a.price)
+        state.parcelas.sort((a, b) => Number(b.price) - Number(a.price))
       }
+    },
+    updatePriceRange: (state, action: PayloadAction<{ min: number; max: number }>) => {
+      state.priceRange.minPrice = action.payload.min;
+      state.priceRange.maxPrice = action.payload.max
+
+    },
+    filterPrice : (state, action: PayloadAction<{ min: number; max: number }>) => {
+        if (state.priceRange) {
+          let filteredPriceParcels = state.allParcelas.filter(
+           (parcela) => Number(parcela.price) >= state.priceRange.minPrice && Number(parcela.price) <= state.priceRange.maxPrice
+           )
+         
+         return {
+           ...state,
+           parcelas: filteredPriceParcels
+         }
+       }
     },
   }
 })
 
-export const { setParcelas, sortParcelas } = parcelasSlice.actions
+export const { setParcelas, sortParcelas, updatePriceRange, filterPrice } = parcelasSlice.actions
 
 export default parcelasSlice.reducer
