1f7cf292f30a6e1852388f59b28cd55e52549618
LucasRibotta <lucasribotta15@gmail.com>
2023-07-04 00:34:16 -0300

    fix router index auth

--- Server/router/index.ts
+++ Server/router/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
 
 import authRoutes from "./auth"
-import authentication from "./authentication"
+
 
 const router = Router()
 
@@ -12,38 +12,6 @@ const PaymentService = require("../services/PaymentService");
 const PaymentInstance = new PaymentController(new PaymentService());
 //---------------------------------------------
 
-router.get("/profile", (req, res) => {
-  console.log(req.user)
-  const userAuth = {
-    user: req.app.locals.user.name,
-    accessLevel: req.app.locals.user.accessLevel,
-    loggedIn: true,
-    error: []
-  }
-  res.status(200).json(userAuth)
-})
-
-router.get("/profileFail", (req, res) => {
-  const usuario = req.flash("error")[0]
-  let usuarioFail = ""
-  if (res.locals.signinMessage.length !== 0)
-    usuarioFail = res.locals.signinMessage[0]
-  else usuarioFail = res.locals.signupMessage[0]
-
-  const userAuthU = {
-    user: usuario,
-    loggedIn: true,
-    error: usuarioFail
-  }
-  res.status(200).json(userAuthU)
-})
-
-router.get("/logout", (req, res, next) => {
-  req.logOut(function (err) {
-    if (err) next(err)
-    res.status(200).json({ user: "vacio", error: [] })
-  })
-})
 
 router.use("/auth", authRoutes)
 
