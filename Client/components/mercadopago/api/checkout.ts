1ebea99777046e18c17f01879b1618a654f78fd3
LucasRibotta <lucasribotta15@gmail.com>
2023-07-05 19:55:50 -0300

    change data unitiprice model.ts

--- Client/components/mercadopago/api/checkout.ts
+++ Client/components/mercadopago/api/checkout.ts
@@ -12,7 +12,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const product: IProduct = req.body.product; //ESTA ES LA LINEA DONDE VA EL PRODUCTO
     
-    const URL = "https://pf-parcela.vercel.app/"; //DESPUES CAMBIAR POR EL DE PRODUCCION / UTILIZAR NGROK
+    const URL = "https://pf-parcela-front.vercel.app/"; //DESPUES CAMBIAR POR EL DE PRODUCCION / UTILIZAR NGROK
 
     try {
       const preference: CreatePreferencePayload = {
