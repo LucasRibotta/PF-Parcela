e2d07af787c47435aebb052351c71e76a40d8aa3
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-07-06 00:59:12 -0500

    feat(Admin): render parcel info

--- Client/components/Admin/Admin.tsx
+++ Client/components/Admin/Admin.tsx
@@ -1,194 +1,208 @@
+"use client"
 import Link from "next/link"
 import PrivateRoute from "../PrivateRoute/PrivateRoute"
+import { useAppDispatch, useAppSelector } from "@/redux/hooks"
+import Card from "./Card"
+import { useEffect } from "react"
+import { setParcelas } from "@/redux/features/parcelSlice"
+import { useGetParcelasQuery } from "@/redux/services/parcelApi"
 
 export default function AdminDash() {
-    return (
-      <PrivateRoute>
-        <div className="pt-[6.8rem] h-screen flex bg-gray-800">
-          <div className="flex flex-col items-center w-40 h-full overflow-hidden text-gray-400 bg-gray-900 rounded">
-            <a className="flex items-center w-full px-3 mt-3" href="#">
-              <svg
-                className="w-8 h-8 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+  const { data, error, isLoading, isFetching } = useGetParcelasQuery("")
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    if (data && Array.isArray(data)) {
+      dispatch(setParcelas(data))
+    }
+  }, [data, dispatch])
+
+  const user = useAppSelector((state) => state.user.userData)
+  const parcelas = useAppSelector((state) => state.parcelas.allParcelas)
+
+  return (
+    <PrivateRoute>
+      <div className="pt-[6.8rem] flex bg-gray-800">
+        <div className="flex flex-col items-center w-40 h-full overflow-hidden text-gray-400 bg-gray-900 rounded">
+          <a className="flex items-center w-full px-3 mt-3" href="#">
+            <svg
+              className="w-8 h-8 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+            </svg>
+            <span className="ml-2 text-sm font-bold">Parcels</span>
+          </a>
+          <div className="w-full px-2">
+            <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
+              <a
+                className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+                href="#"
               >
-                <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-              </svg>
-              <span className="ml-2 text-sm font-bold">Parcels</span>
-            </a>
-            <div className="w-full px-2">
-              <div className="flex flex-col items-center w-full mt-3 border-t border-gray-700">
-                <a
-                  className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-                  href="#"
+                <svg
+                  className="w-6 h-6 stroke-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  <svg
-                    className="w-6 h-6 stroke-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                    />
-                  </svg>
-                  <span className="ml-2 text-sm font-medium">Gráficos</span>
-                </a> 
-                <a
-                  className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-                  href="#"
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+                <span className="ml-2 text-sm font-medium">Gráficos</span>
+              </a>
+              <a
+                className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+                href="#"
+              >
+                <svg
+                  className="w-6 h-6 stroke-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  <svg
-                    className="w-6 h-6 stroke-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                  <span className="ml-2 text-sm font-medium">Buscar</span>
-                </a> 
-  
-                <div className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-backspace-reverse-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M0 3a2 2 0 0 1 2-2h7.08a2 2 0 0 1 1.519.698l4.843 5.651a1 1 0 0 1 0 1.302L10.6 14.3a2 2 0 0 1-1.52.7H2a2 2 0 0 1-2-2V3zm9.854 2.854a.5.5 0 0 0-.708-.708L7 7.293 4.854 5.146a.5.5 0 1 0-.708.708L6.293 8l-2.147 2.146a.5.5 0 0 0 .708.708L7 8.707l2.146 2.147a.5.5 0 0 0 .708-.708L7.707 8l2.147-2.146z" />
-                  </svg>
-                  <Link href="/form">
-                    <button type="button" className="ml-2 text-sm font-medium">
-                      Crear Parcela
-                    </button>
-                  </Link>
-                </div>
-  
-                <a
-                  className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-                  href="#"
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+                <span className="ml-2 text-sm font-medium">Buscar</span>
+              </a>
+
+              <div className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-backspace-reverse-fill"
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    className="w-6 h-6 stroke-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
-                    />
-                  </svg>
-                  <span className="ml-2 text-sm font-medium">Docs</span>
-                </a> 
+                  <path d="M0 3a2 2 0 0 1 2-2h7.08a2 2 0 0 1 1.519.698l4.843 5.651a1 1 0 0 1 0 1.302L10.6 14.3a2 2 0 0 1-1.52.7H2a2 2 0 0 1-2-2V3zm9.854 2.854a.5.5 0 0 0-.708-.708L7 7.293 4.854 5.146a.5.5 0 1 0-.708.708L6.293 8l-2.147 2.146a.5.5 0 0 0 .708.708L7 8.707l2.146 2.147a.5.5 0 0 0 .708-.708L7.707 8l2.147-2.146z" />
+                </svg>
+                <Link href="/form">
+                  <button type="button" className="ml-2 text-sm font-medium">
+                    Crear Parcela
+                  </button>
+                </Link>
               </div>
-              <div className="flex flex-col items-center w-full mt-2 border-t border-gray-700">
-                <a
-                  className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-                  href="#"
+
+              <a
+                className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+                href="#"
+              >
+                <svg
+                  className="w-6 h-6 stroke-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  <svg
-                    className="w-6 h-6 stroke-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                    />
-                  </svg>
-                  <span className="ml-2 text-sm font-medium">Productos</span>
-                </a>
-                <a
-                  className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-                  href="#"
-                >
-                  <svg
-                    className="w-6 h-6 stroke-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                    />
-                  </svg>
-                  <span className="ml-2 text-sm font-medium">Ajustes</span>
-                </a>
-                <a
-                  className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-                  href="#"
-                >
-                  <svg
-                    className="w-6 h-6 stroke-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                    />
-                  </svg>
-                  <span className="ml-2 text-sm font-medium">Mensajes</span>
-                  <span className="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></span>
-                </a>
-              </div> 
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+                  />
+                </svg>
+                <span className="ml-2 text-sm font-medium">Docs</span>
+              </a>
             </div>
-          </div>
-          <div className="flex flex-col flex-1 p-4  bg-gray-800">
-            <h1 className="text-2xl font-bold text-white">Dashboard</h1>
-            <div className="flex flex-wrap mt-6 ">
-              <div className="w-full px-2 lg:w-1/2 xl:w-1/3 mb-2">
-                <div className="p-4 bg-white rounded shadow">
-                  <h2 className="text-lg font-bold">Estadísticas</h2>
-                  {/* Aquí puedes agregar tus componentes de estadísticas */}
-                </div>
-              </div>
-              <div className="w-full px-2 lg:w-1/2 xl:w-1/3 mb-2">
-                <div className="p-4 bg-white rounded shadow">
-                  <h2 className="text-lg font-bold">Notificaciones</h2>
-                  {/* Aquí puedes agregar tus componentes de notificaciones */}
-                </div>
-              </div>
-              <div className="w-full px-2 lg:w-1/2 xl:w-1/3 mb-2">
-                <div className="p-4 bg-white rounded shadow">
-                  <h2 className="text-lg font-bold">Actividades recientes</h2>
-                  {/* Aquí puedes agregar tus componentes de actividad reciente */}
-                </div>
-              </div>
+            <div className="flex flex-col items-center w-full mt-2 border-t border-gray-700">
+              <a
+                className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+                href="#"
+              >
+                <svg
+                  className="w-6 h-6 stroke-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="ml-2 text-sm font-medium">Productos</span>
+              </a>
+              <a
+                className="flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+                href="#"
+              >
+                <svg
+                  className="w-6 h-6 stroke-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                  />
+                </svg>
+                <span className="ml-2 text-sm font-medium">Ajustes</span>
+              </a>
+              <a
+                className="relative flex items-center w-full h-12 px-3 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
+                href="#"
+              >
+                <svg
+                  className="w-6 h-6 stroke-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                  />
+                </svg>
+                <span className="ml-2 text-sm font-medium">Mensajes</span>
+                <span className="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></span>
+              </a>
             </div>
           </div>
         </div>
-      </PrivateRoute>
-    )
-  }
-  
+        <div className="flex flex-col flex-1 p-4  bg-gray-800">
+          <h2 className="text-2xl font-bold text-white">Parcelas activas</h2>
+          <div className="grid grid-cols-3 mt-6 ">
+            {parcelas && parcelas.map(el => {
+              if (el.deleted === false) {
+                return <div className=" w-full flex " key={el._id}><Card name={el.name} id={el._id} /></div>
+              }
+            })}
+
+          </div>
+
+          <h2 className="text-2xl font-bold text-white">Parcelas Ocultas</h2>
+          <div className="grid grid-cols-3 mt-6 ">
+            {parcelas && parcelas.map(el => {
+              if (el.deleted === true) {
+                return <div className=" w-full flex " key={el._id}><Card name={el.name} id={el._id} /></div>
+              }
+            })}
+
+          </div>
+        </div>
+      </div>
+    </PrivateRoute>
+  )
+}
