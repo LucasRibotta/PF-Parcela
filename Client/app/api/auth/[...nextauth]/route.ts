5e6c66bb6aa4f0067048ad7ee2844da56a679388
Ismael <ismael2095951@gmail.com>
2023-07-09 20:42:41 -0300

    feat([...nextauth/route) implementation saved google user in DB

--- Client/app/api/auth/[...nextauth]/route.ts
+++ Client/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,17 @@
 import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
 import axios from "axios"
-import 'dotenv/config'
+import 'dotenv/config';
+import {User, Account, Profile} from "next-auth"
+import { AdapterUser } from "next-auth/adapters";
+
+interface profi extends Profile {
+  given_name?: string,
+  family_name?: string,
+
+}
 
 const handler = NextAuth({
   providers: [
@@ -38,16 +47,45 @@ const handler = NextAuth({
             response_type: "code"
           }
         }
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
     })
   ],
+  
   callbacks: {
     jwt({ account, user, profile, session, token }) {
-      if (user) token.user = user
+      if (user) {
+        token.user = user  
+      }
       return token
     },
     session({ session, token }) {
       session.user = token.user as any
       return session
+    },
+    async signIn({user, account, profile}: {user: User | AdapterUser,account: Account | null, profile?: profi | undefined}): Promise<boolean> {
+    
+      const userProvider= {
+        password: user.id,
+        name: profile?.given_name,
+        lastname: profile?.family_name,
+        email: user.email,  
+        image: user.image,
+        provider: account?.provider,
+        accessToken: account?.access_token,
+      }
+      try {
+        const response = await axios.post(
+          //"http://localhost:3001/register", userProvider
+          "https://pf-parcela-production.up.railway.app/register", userProvider
+        )   
+        return true
+      } catch (error) {
+        console.log(error);
+        return false    
+      }
     }
   },
   pages: {
