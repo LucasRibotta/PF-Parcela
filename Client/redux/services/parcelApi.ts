0295bfc615da00636adc4dcfb37f6a62828cb5bd
LucasRibotta <lucasribotta15@gmail.com>
2023-07-12 19:53:21 -0300

    change data url prueba

--- Client/redux/services/parcelApi.ts
+++ Client/redux/services/parcelApi.ts
@@ -17,7 +17,7 @@ interface Parcela {
 export const parcelApi = createApi({
   reducerPath: "parcelApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://pf-parcela-production-2bf5.up.railway.app/"
+    baseUrl: "https://pf-parcela-production-2bf5.up.railway.app/api/auth/"
   }),
   endpoints: (builder) => ({
     getParcelas: builder.query<Parcela[], string>({
