3ab1dfb306009879ff52a136f2b3c9759157fdda
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-07-12 01:24:51 -0500

    refactor(passwordUpdate): from div to form for send automatic

--- Client/app/userDataRegister/passwordUpdate/page.tsx
+++ Client/app/userDataRegister/passwordUpdate/page.tsx
@@ -18,7 +18,8 @@ export default function PasswordUpdate() {
     setPsw(value);
   }
 
-  const handleUpdate = () => {
+  const handleUpdate = (event: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
 
     if (dataUser) {
       const { _id, name, lastname, phone, date, email, password, isAdmin, isCompany } = dataUser
@@ -28,14 +29,14 @@ export default function PasswordUpdate() {
       updateUser({ id: _id, data })
     }
 
-
+    setTimeout(() => {
+      setPsw("");
+    },2000)
 
   }
 
-
-
   return (
-    <div className="mt-[1rem]">
+    <form onSubmit={handleUpdate} className="mt-[1rem]">
       <div className="flex items-center gap-2">
         <Link href="/userDataRegister">
           <IoChevronBackOutline className="h-6 w-6 stroke-current" />
@@ -53,6 +54,7 @@ export default function PasswordUpdate() {
               className="peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-4 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-[#51a8a1] focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
               placeholder=" "
               onChange={handleChange}
+              value={psw}
             />
             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-[#51a8a1] peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-[#51a8a1] peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-[#51a8a1] peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
               Contraseña
@@ -63,6 +65,6 @@ export default function PasswordUpdate() {
           Guardar cambios
         </button>
       </div>
-    </div>
+    </form>
   )
 }
