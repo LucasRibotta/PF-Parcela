54c1785c4b4dbdecb1000e7845e77c7068505e00
xxnahokxx <johan.s_vanegas@hotmail.com>
2023-07-16 23:03:36 -0500

    feat(Contact and Dashboard Admin): front Message

--- Client/app/layout.tsx
+++ Client/app/layout.tsx
@@ -44,8 +44,6 @@ export default function RootLayout({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <link href="https://chatrace.com/webchat/?p=1898889&ref=1587905477489" rel="stylesheet"/>
-
       </head>
       <body className={inter.className}>
         <ProvidersWrapper>
