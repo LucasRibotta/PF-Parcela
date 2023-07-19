012b013a1e4fff027f061c54ec59033e7c9ca055
percy <percy.u.lomas@gmail.com>
2023-07-19 10:45:39 -0500

    refactor-formS&validate-validations correction

--- Client/components/Validate/validate.ts
+++ Client/components/Validate/validate.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 export const validate = z.object({
-	name: z.string().nonempty("* El nombre es requerido"),
+	name: z
+    .string()
+    .nonempty("* El nombre es requerido")
+    .refine((val) => isNaN(Number(val)), {
+      message: "* El nombre no puede ser un número",
+    }),
 	lote: z
 		.number()
 		.refine((val) => val !== 0 || val === null, {
@@ -17,7 +22,13 @@ export const validate = z.object({
 			message: "* El precio es requerido",
 		}),
 	location: z.string().nonempty("* La ubicación es requerida"),
-	description: z.string().nonempty("* La descripción es requerida"),
+	description: z
+    .string()
+    .nonempty("* La descripción es requerida")
+    .refine((val) => isNaN(Number(val)), {
+      message: "* La descripción no puede ser un número",
+    }),
+
 	image: z.array(z.string()).min(1, "* Al menos una imagen es requerida"),
 	user: z.string(),
 });
